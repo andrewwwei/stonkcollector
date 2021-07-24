1035f2fba516da01be2099cba3b7ceec660fdba7
calvin4770 <calvin4770@gmail.com>
2021-07-24 21:43:48 +0800

    Added comments and fixed some bugs

--- collector.py
+++ collector.py
@@ -6,27 +6,32 @@ class HistDataCollector:
         self.tokens = tokens
         api_key, secret_key = self.load_keys() # put api key in first line, secret key in 2nd line of keys.txt
         self.client = Client(api_key, secret_key)
-        
+    
+    # get latest data and save it in csv format
+    # indices 0-10: open time, open, high, low, close, volume, close time, quote asset vol, # of trades,
+    # taker buy base asset vol, taker buy quote asset vol
     def save_data(self):
         for t in self.tokens:
             pair = (t + "usdt").upper()
             f = open("data/{}.csv".format(t), "w+")
-            data = self.client.get_historical_klines(pair, Client.KLINE_INTERVAL_1WEEK, start_str=0)
+            data = self.client.get_historical_klines(pair, Client.KLINE_INTERVAL_15MINUTE, start_str=0)
             for x in data:
-                n = 6
-                for i in range(1, n):
+                n = 11
+                for i in range(0, n):
                     f.write(str(x[i]))
                     if i < n - 1: f.write(",")
                 f.write("\n")
             f.close()
-
+    
+    # load data from stored csv files
     def load_data(self):
         df_list = []
         for t in self.tokens:
             df = pd.read_csv("data/{}.csv".format(t))
             df_list.append(df)
         return df_list
-
+    
+    # load secret key for binance
     def load_keys(self):
         f = open("keys.txt", "r")
         api_key = f.readline().strip()
--- data_processing.py
+++ data_processing.py
@@ -3,29 +3,32 @@ from ta.trend import *
 from ta.momentum import *
 
 def rsi(df):
-    r = RSIIndicator(df.iloc[:, 3], window=10)
-    print(df.iloc[:, 3])
+    r = RSIIndicator(df.iloc[:, 4], window=10)
     return r.rsi()
 
 def obv(df):
-    o = OnBalanceVolumeIndicator(df.iloc[:, 4], df.iloc[:, 5])
+    o = OnBalanceVolumeIndicator(df.iloc[:, 5], df.iloc[:, 6])
     return o.on_balance_volume()
 
 def adl(df):
-    a = AccDistIndexIndicator(df.iloc[:, 2], df.iloc[:, 3], df.iloc[:, 4], df.iloc[:, 5])
+    a = AccDistIndexIndicator(df.iloc[:, 3], df.iloc[:, 4], df.iloc[:, 5], df.iloc[:, 6])
     return a.acc_dist_index()
 
 def adx(df):
-    a = ADXIndicator(df.iloc[:, 2], df.iloc[:, 3], df.iloc[:, 4])
+    a = ADXIndicator(df.iloc[:, 3], df.iloc[:, 4], df.iloc[:, 5])
     return a.adx()
 
 def output_labels(df):
     r = rsi(df)
     labels = []
+    prices = df.iloc[:, 1]
     mult = 1.002
     shift = 5
+    
+    # generate labels for data
     for i in range(10, len(r) - shift):
-        if r[i] < 20 and r[i + shift] > mult * r[i]:
+        # trash criteria
+        if r[i] < 20 and prices[i + shift] > mult * prices[i]:
             labels.append(1)
         else:
             labels.append(0)
